14cb6a38f416064f8e6aa05bc8afce97a06063f3
Bernhard M. Wiedemann <bernhard+gitcommit lsmod.de>
2023-07-30 19:36:45 +0200

    Add versionclassifier

---
+++ tools/versionclassifier.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl -w
+# SPDX-License-Identifier: GPL-2.0-only
+use strict;
+use JSON::XS;
+
+sub cmpverpart($$)
+{ my ($v1, $v2) = @_;
+    return 0 if $v1 eq $v2;
+    my @v1 = split("[-._+~]", $v1);
+    my @v2 = split("[-._+~]", $v2);
+    for my $i (0..$#v1) {
+        if($v1[$i] ne $v2[$i]) {
+            return 1+$i;
+        }
+    }
+    return 63;
+}
+
+sub cmpversion($$)
+{ my ($v1, $v2) = @_;
+    if($v1->{epoch} ne $v2->{epoch}) {
+        return 1;
+    } elsif(my $p = cmpverpart($v1->{ver}, $v2->{ver})) {
+        return $p;
+    } elsif(my $p = cmpverpart($v1->{rel}, $v2->{rel})) {
+        return $p+64;
+    } else {
+        return 0;
+    }
+}
+
+
+my @files = <cache/*/primary.pkgs>;
+#print "@files\n";
+my @jsons = ();
+foreach my $fname (@files) {
+    open(my $f, "<", $fname) or die "error reading $fname";
+    local $/=undef;
+    my $json = <$f>;
+    push(@jsons, decode_json $json);
+}
+
+foreach my $pkg (sort keys (%{$jsons[0]})) {
+    my $p0 = $jsons[0]->{$pkg};
+    my $p1 = $jsons[1]->{$pkg};
+    my $vercmp = cmpversion($p0->{version}, $p1->{version});
+    print "$pkg $p0->{version}{ver} $p1->{version}{ver} $vercmp\n";
+}
+
