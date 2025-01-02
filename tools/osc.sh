5683f9c4d66b07c07256f2a1f989b6c4942f62c9
Bernhard M. Wiedemann <bwiedemann@suse.de>
2025-01-02 08:31:29 +0000

    Use curl's cookiejar to simplify things
    and make it handle expired cookies

--- tools/osc.sh
+++ tools/osc.sh
@@ -3,14 +3,7 @@
 
 #dry=echo
 : ${apiurl:=https://api.opensuse.org}
-cookie=$(perl -ne 'm/(openSUSE_session=[0-9a-f]+);.*domain=..opensuse.org/ && print $1' ~/.local/state/osc/cookiejar)
-curl="curl"
-if [ -n "$cookie" ] ; then
-    curl="$curl --header Cookie:$cookie"
-else
-    curl="$curl -n" # use user:password auth
-fi
-
+curl="curl -n --cookie $HOME/.local/state/osc/cookiejarcurl --cookie-jar $HOME/.local/state/osc/cookiejarcurl"
 
 function osc_api
 {
